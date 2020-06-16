98924a74fca5461493233d4b9906769b5d0c41de
MichaelBakshi <62958898+MichaelBakshi@users.noreply.github.com>
2020-06-16 23:07:56 +0300

    Add files via upload

---
+++ 14.06.Homework/14.06.Homework/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._06.Homework
+{
+    class Computer
+    {
+        public string model;
+        public int price;
+        public int numberOfProcessors;
+        public float screenSize;
+        public bool isTurnOn;
+        public bool isTurnOff;
+
+
+        public int TellMeThePrice ()
+        {
+            return price;
+        }
+      
+        public float TellMeTheScreenSize()
+        {
+            return screenSize;
+        }
+        public void TurnOn()
+        {
+            
+        }
+
+        public void TurnOff()
+        {
+
+        }
+
+        public void AddProcessor()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"";
+        }
+
+    }
+}
---
+++ 14.06.Homework/14.06.Homework/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._06.Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
